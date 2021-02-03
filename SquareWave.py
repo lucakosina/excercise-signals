3ab2ce484ce4b1d00f3a84ecf29af4167491cc62
lucakosina <luca.kosina@freenet.de>
2021-02-03 22:01:10 +0100

    Update SquareWave.py

--- SquareWave.py
+++ SquareWave.py
@@ -14,7 +14,7 @@ plt.show()
 f_max_3 = (1/st_3)/2
 p3 = 20*np.log10(np.abs(np.fft.rfft(signal_3)))
 f3 = np.linspace(0, f_max_3, len(p3))
-k3 = 1 + 14*np.sinc(f3*6.5 - 0.7)
+k3 = 2.4683 + 2.4084*np.exp(-1.3239*f3 + 2.1310) * np.cos(1.4990e-04*f3 - 1.2201)
 plt.plot(f3, p3, '-', f3, k3, '-.')
 plt.title("Spectrum of a square signal")
 plt.ylabel("Spectral Intensity / Decibel")
@@ -22,7 +22,7 @@ plt.xlabel("Frequency")
 plt.show()
 
 
-def func(x, a, b, c, d):
-    return a * np.sinc(b*x-c) + d 
-popt, pcov = sp.optimize.curve_fit(func, f3, p3)
+def func(x, a, b, c, d, e, f):
+    return a * np.exp(-b*x+c) * np.cos(d*x+e) + f
+popt, pcov = sp.optimize.curve_fit(func, f3, p3, maxfev = 5000)
 print(popt)
