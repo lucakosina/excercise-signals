f2b34d4f88c4eabca5542b6852a830746425f0d9
lucakosina <luca.kosina@freenet.de>
2021-02-04 21:08:03 +0100

    Update SawtoothWave.py

--- SawtoothWave.py
+++ SawtoothWave.py
@@ -19,15 +19,16 @@ plt.show()
 f_max_4 = (1/st_4)/2
 p4 = 20*np.log10(np.abs(np.fft.rfft(signal_4)-1))
 f4 = np.linspace(0, f_max_4, len(p4))
-k4 = 2.5320 + 1.53636*np.sinc(f4*0.75085-1.53636) 
+delta_f_4 = f_max_4/(len(p4)-1)
+k4 = 42.1127*signal.unit_impulse(np.shape(f4), idx = int(1/delta_f_4)) 
+#k4 = 2.4449 + 9.3226*np.sinc(f4*0.9999-0.9999) * 4.2549*signal.unit_impulse(np.shape(f4), idx = int(1/delta_f_4)) 
 plt.plot(f4, p4, '-', f4, k4, '-.')
 plt.title("Spectrum of a sawtooth signal")
 plt.ylabel("Spectral Intensity / Decibel")
 plt.xlabel("Frequency")
 plt.show()
 
-def func(x, a, b, c, d):
-    return a * np.sinc(b*x-c) + d 
+def func(x, a):
+    return a * signal.unit_impulse(np.shape(f4), idx = int(1/delta_f_4)) 
 popt, pcov = sp.optimize.curve_fit(func, f4, p4)
 print(popt)
-
