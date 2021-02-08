d249c366e986a61731e65bc542d078a80fdb857b
lucakosina <luca.kosina@freenet.de>
2021-02-08 13:13:24 +0100

    Update SawtoothWave.py

--- SawtoothWave.py
+++ SawtoothWave.py
@@ -23,10 +23,19 @@ delta_f_4 = f_max_4/(len(p4)-1)
 k4 = [0] * 201
 k4[0] = 1 * 21
 k4[10] = 1 * 42
-k4[20] = 1 * 36
-k4[30] = 1 * 32.5
-k4[40] = 1 * 30.5
-k4[50] = 1 * 28.5
+k4[20] = 1 * 37
+k4[30] = 1 * 33
+k4[40] = 1 * 30
+k4[50] = 1 * 28
+k4[60] = 1 * 27
+k4[70] = 1 * 26
+k4[80] =1 * 25
+k4[90] = 1 * 24.5
+k4[100] = 1 * 23.5
+k4[110] =  1 * 23
+#x = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+#for elem in x:
+#    k4[elem] = 1
 
 #k4 = 42.1127*signal.unit_impulse(np.shape(f4), idx = int(1/delta_f_4)) 
 plt.plot(f4, p4, '-', f4, k4, '-.')
@@ -35,7 +44,4 @@ plt.ylabel("Spectral Intensity / Decibel")
 plt.xlabel("Frequency")
 plt.show()
 
-def func(x, a):
-    return a * signal.unit_impulse(np.shape(f4), idx = int(1/delta_f_4)) 
-popt, pcov = sp.optimize.curve_fit(func, f4, p4)
-print(popt)
+
