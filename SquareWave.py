40e46f556d6031957341d058501f25235044428a
lucakosina <luca.kosina@freenet.de>
2021-02-22 19:58:46 +0100

    Update SquareWave.py

--- SquareWave.py
+++ SquareWave.py
@@ -6,7 +6,7 @@ import math
 
 #square
 st_3 = 0.001
-time = np.arange(0, 500, st_3)
+time = np.arange(0, 10, st_3)
 signal_3 = signal.square(time)
 plt.plot(time, signal_3)
 plt.title("Square")
@@ -16,7 +16,7 @@ f_max_3 = (1/st_3)/2
 p3 = 20*np.log10(np.abs(np.fft.rfft(signal_3)))
 f3 = np.linspace(0.0001, f_max_3, len(p3))
 frequency = f3 * (1/st_3) / 25001
-k3 = 90 * np.sinc(0.9*frequency + 0.4) + 37
+k3 = 120 * np.sinc(0.5*frequency + 0.56) + 5
 #k3 = 1000 * np.sinc(0.2*f3+4) + 23
 plt.plot(f3, p3, '-', f3, k3, '.-')
 plt.title("Spectrum of a square signal")
